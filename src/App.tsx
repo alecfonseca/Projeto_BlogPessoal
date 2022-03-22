c925f538fa0dce04ebfb313bb266496f4a43078b
Alessandro Castro <97323030+alecfonseca@users.noreply.github.com>
2022-03-22 19:00:29 -0300

    Implementando corpo site

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Navibar from './components/estaticos/navbar/Navibar';
+import Footer from './components/estaticos/footer/Footer';
+import { Grid } from '@material-ui/core';
 import './App.css';
 import Home from './paginas/home/Home';
 
 function App() {
   return (
-    <Home />
+      <>
+         <Navibar />
+         <Home />
+         <Footer />
+      </>
   );
 }
 

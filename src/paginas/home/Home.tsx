3253e09700202d25b80c13f2319cfdbcbb23b6d1
Alessandro Castro <97323030+alecfonseca@users.noreply.github.com>
2022-03-22 10:39:16 -0300

    Introdução ao React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+          <h1 className="titulo">Home</h1>
+          <img src="https://images.financialexpress.com/2017/08/Blogs.jpg" alt="Imagem principal" className="img" />
+        </>
+    );
+}
+
+export default Home;
